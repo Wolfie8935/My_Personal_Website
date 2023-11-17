5ee8cdfa25a5e0ebebdfb9ada84ff7ce60453181
Aman Goel <124771341+Wolfie8935@users.noreply.github.com>
2023-11-17 15:57:10 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: iPortfolio
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 
 (function() {
   "use strict";
